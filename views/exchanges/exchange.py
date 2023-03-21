1e44e01b4d304b32d5fbaf1575c43211992d48dd
Quoc Anh Tran <64301034+19020214@users.noreply.github.com>
2023-03-21 09:53:07 +0700

    add-exchange-popup

--- views/exchanges/exchange.py
+++ views/exchanges/exchange.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.modalview import ModalView
 from kivy.metrics import dp, sp
 from kivy.utils import rgba, QueryDict
 from kivy.clock import Clock, mainthread
@@ -29,7 +30,7 @@ class Exchange(BoxLayout):
                 "id": "35235",
                 "title": "KRAKEN",
                 "source": "assets/imgs/kraken_logo.png",
-                "connected": True,
+                "connected": False,
                 "keys": {
                     "key": "/sk12162sZfc6L7kohoUg7dpPOQfV88ejSwNUpLHvi2UhaX4HwmzT0BX",
                     "secret": "ejLSo7/JmSeBeBW5y33vxJC7QoK/o7yJyYyl9eHyXONJn45Wt/Q639xboW399BJiWf2eiefFuEqQ0qOZ8Pi/mQ=="
@@ -39,7 +40,7 @@ class Exchange(BoxLayout):
                 "id": "35215",
                 "title": "OKCOIN",
                 "source": "assets/imgs/ok-coin.png",
-                "connected": True,
+                "connected": False,
                 "keys": {
                     "key": "2b8248a6-3dfb-4f40-b44f-cfa32f18e195",
                     "secret": "66072F864FC529751ED9A0BA9049067E",
@@ -54,13 +55,14 @@ class Exchange(BoxLayout):
         exc.clear_widgets()
 
         for e in exchanges:
-            ex = ConnectedExchange()
-            ex.title = e['title']
-            ex.source = e['source']
-            ex.connected = e['connected']
-            ex.keys = e['keys']
+            if e['connected']:
+                ex = ConnectedExchange()
+                ex.title = e['title']
+                ex.source = e['source']
+                ex.connected = e['connected']
+                ex.keys = e['keys']
 
-            grid.add_widget(ex)
+                grid.add_widget(ex)
 
             ev = ExchangeTile()
             ev.title = e['title']
@@ -70,33 +72,6 @@ class Exchange(BoxLayout):
 
             exc.add_widget(ev)
 
-    @mainthread
-    def on_coins(self, inst, mkts):
-        return
-        grid = self.ids.gl_currencies
-        popular = self.ids.gl_popular
-        popular.clear_widgets()
-        grid.clear_widgets()
-
-        for v in mkts:
-            a = ListTile()
-            a.text = str(v['symbol']).upper()
-            a.source = v['image']
-            a.price = v['current_price']
-            a.price_change = v['market_cap_change_percentage_24h']
-            a.data = v
-            grid.add_widget(a)
-
-        for v in mkts:
-            if str(v['symbol']) in self.popular:
-                a = Asset()
-                a.text = str(v['symbol']).upper()
-                a.source = v['image']
-                a.price = round(v['current_price'], 2)
-                a.price_change = v['market_cap_change_percentage_24h']
-                a.data = v
-                a.height = Window.height*.2
-                popular.add_widget(a)
 
 class BaseExchange(BoxLayout):
     title = StringProperty("")
@@ -111,5 +86,22 @@ class ConnectedExchange(BaseExchange):
         super().__init__(**kw)
 
 class ExchangeTile(BaseExchange):
+    def __init__(self, **kw) -> None:
+        super().__init__(**kw)
+
+    def get_connect(self):
+        if self.connected:
+            return
+
+        ne = NewExchange()
+        ne.source = self.source
+
+        if self.keys.get("passphrase"):
+            ne.passphrase = True
+        ne.open()
+
+class NewExchange(ModalView):
+    passphrase = BooleanProperty(False)
+    source = StringProperty("")
     def __init__(self, **kw) -> None:
         super().__init__(**kw)
